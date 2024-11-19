2355a107d3b904fbc4335825dff23d677e51e3c3
Eitan Michaelson <eitan.michaelson@intel.com>
2024-11-19 21:56:19 +0200

    Delete sdk_install.sh

--- sdk_install.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p workspace
-cd workspace
-curl -sSL -H "Authorization: token $(dt github print-token https://github.com/intel-innersource/firmware.ethernet.imcv2)" -H "Cache-Control: no-store" "https://raw.githubusercontent.com/intel-innersource/firmware.ethernet.imcv2/main/scripts/imcv2_boot_strap.sh" | bash -s -- -b main
